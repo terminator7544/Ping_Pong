39da9085a0ea7c26caed7677db0e61f0881863fc
DESKTOP-M1GTCJA\Ученик <1@1.com>
2024-05-12 12:31:19 +0300

    Classes added

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, image, palyer_x, palyer_y, win):
+        super().__init__()
+        self.image = image
+        self.win = win
+        self.rect = self.image.get_rect()
+        self.rect.x = palyer_x
+        self.rect.y = palyer_y
+        self.obj_y = palyer_y
+        self.obj_x = palyer_x
+
+class Player(GameSprite):
+    movable = {"left":{0:K_w,
+                       1:K_s},
+               'right':{0:K_UP,
+                        1:K_DOWN}}
+    def __int__(self,image, palyer_x, palyer_y, win,move):
+        super().__init__(image, palyer_x, palyer_y, win)
+        self.keys = [Player.movable[move][0],Player.movable[move][1]]
+
+class Ball(GameSprite):
+    def __init__(self,image, palyer_x, palyer_y, win):
+        super().__init__(image, palyer_x, palyer_y, win)
+        self.speed_x = 1
+        self.speed_y = 1
+
 win = display.set_mode((700, 500))
 display.set_caption("Ping_Pong")
 clock = time.Clock()
